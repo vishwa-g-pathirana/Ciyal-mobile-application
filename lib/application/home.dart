8da44217030bdc6a327e72dbb34490f92f6fb369
vishwa G pathirana <68497315+vishwa-g-pathirana@users.noreply.github.com>
2021-08-02 20:02:21 +0530

    2021/8/2

--- lib/application/home.dart
+++ lib/application/home.dart
@@ -168,6 +168,8 @@ class home extends StatelessWidget {
     );
   }
 
+  //offers card
+
   Widget promoCard(image) {
     return AspectRatio(
       aspectRatio: 2.62 / 3,
@@ -191,27 +193,124 @@ class home extends StatelessWidget {
       ),
     );
   }
+//product card
 
+  //image
   Widget productCard(image) {
     return AspectRatio(
       aspectRatio: 2.62 / 1.3,
-      child: Container(
-        margin: EdgeInsets.only(top: 15.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
+      child: Row(
+        children: [
+          Container(
+            width: 170,
+            height: 170,
+            margin: EdgeInsets.only(top: 15.0),
+            decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
-                0.1,
-                0.9
-              ], colors: [
-                Colors.black.withOpacity(.8),
-                Colors.black.withOpacity(.1)
-              ])),
-        ),
+              image:
+                  DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomRight,
+                      stops: [
+                        0.1,
+                        0.9
+                      ],
+                      colors: [
+                        Colors.black.withOpacity(.8),
+                        Colors.black.withOpacity(.1)
+                      ])),
+            ),
+          ),
+
+          //lable
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient:
+                        LinearGradient(begin: Alignment.bottomRight, stops: [
+                      0.1,
+                      0.9
+                    ], colors: [
+                      Colors.grey.withOpacity(.1),
+                      Color.fromRGBO(244, 243, 243, 1).withOpacity(0.09),
+                    ])),
+                width: 200,
+                height: 170,
+                margin: EdgeInsets.only(top: 15.0),
+                padding: EdgeInsets.only(right: 10, top: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Chicken Burger',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'ref 1022010',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Matara Bath kade',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'RS 300/=',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 19,
+                    ),
+                    Text(
+                      'Delivery Free',
+                      style: TextStyle(color: Colors.red[300]),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
