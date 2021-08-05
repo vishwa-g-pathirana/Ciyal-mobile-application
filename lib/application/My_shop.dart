a69beec31ca852c7088eeb158fa8b1f809aa4f85
vishwa G pathirana <68497315+vishwa-g-pathirana@users.noreply.github.com>
2021-08-05 22:39:26 +0530

    Update My_shop.dart
    2021/8/5

--- lib/application/My_shop.dart
+++ lib/application/My_shop.dart
@@ -1,10 +1,152 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class ShopPage extends StatelessWidget {
   const ShopPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: kBackgroundColor,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back_outlined,
+            color: kPrimaryColor,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.more_vert,
+              color: kPrimaryColor,
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20, bottom: 10),
+                  child: Container(
+                    child: Text(
+                      'Hi ! Samadhi K,', //user name
+                      style: TextStyle(
+                        color: kPrimaryColor,
+                        fontSize: 25,
+                        //fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  child: Text(
+                    'Create your online shop', //slogan
+                    style: TextStyle(color: Colors.grey, fontSize: 15),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  margin: EdgeInsets.only(top: 60),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: kPrimaryColor),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      hintText: 'Enter shop name',
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  margin: EdgeInsets.only(top: 40),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: kPrimaryColor),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      hintText: 'Enter owner / owners name',
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  margin: EdgeInsets.only(top: 40),
+                  child: Container(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    height: 150,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                          color: kOrangeColor,
+                        )),
+                    child: TextFormField(
+                      decoration: new InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        hintText: 'Discription',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 40),
+                child: Center(
+                  child: SizedBox(
+                    width: 300,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'continue ',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(kPrimaryColor),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
